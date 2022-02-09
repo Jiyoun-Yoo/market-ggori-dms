3633b0a3a7b81a7028abddcdad90be8c2a4f4534
jiyounyou <younny418@gmail.com>
2022-02-09 23:35:31 +0900

    feat: thymleaf layout

--- src/main/java/com/teamfresh/teamfreshassignment/controller/LoginController.java
+++ src/main/java/com/teamfresh/teamfreshassignment/controller/LoginController.java
@@ -5,16 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class CommonController {
+public class LoginController {
 
-  @GetMapping("common")
+  @GetMapping("login")
   public String common(Model model) {
     model.addAttribute("data", "team fresh!!");
-    return "common/common";
+    return "auth/login";
   }
 
-  @GetMapping("hello")
-  public String hello( ) {
-    return "hello";
-  }
+
 }
